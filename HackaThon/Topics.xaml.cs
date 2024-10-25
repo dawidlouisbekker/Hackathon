aafdaaba14bf339dec636941c37801466858419d
dawid <dawid@DESKTOP-MEL21TQ>
2024-10-25 05:59:55 -0700

    Add project files.

---
+++ HackaThon/Topics.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace HackaThon
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        ItemsView Topics { get; set; };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
+
+public class ItemModel
+{
+    public string UserName { get; set; }
+    public string TopicDetials { get; set; }
+    public string TopicName { get; set; }
+}
